9bebd65324f3722b81b7cf38bbba611db28e02b8
leo-g7 <leo@whyyle.com>
2022-10-27 11:48:01 +0200

    Start register in db

--- iti-community/src/modules/user/services/platform/http/user.commands.http.ts
+++ iti-community/src/modules/user/services/platform/http/user.commands.http.ts
@@ -11,6 +11,7 @@ export class HttpUserCommands extends UserCommands {
   }
 
   register(username: string, password: string): Promise<{ id: string; }> {
+    console.log('test')
     return this.http.post<{ id: string; }>(`${environment.serverBaseUrl}/user`, { username, password }).toPromise();
   }
 
